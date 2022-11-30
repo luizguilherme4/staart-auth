3270a5f187390e2bd4ffbef851e61999aadda961
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2022-11-29 22:43:05 -0300

    criando novas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/authContext";
+import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
+import { UserProfile } from "./pages/UserProfile";
 
 function App() {
   return (
     <AuthProvider>
-      <Signup />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserProfile />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<div><h1>Rota não encontrada</h1></div>}></Route>
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
